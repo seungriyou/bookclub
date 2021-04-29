194d278f5fd96324f26c1afddc1bc5f838becdaa
myatta <43639741+myatta@users.noreply.github.com>
2021-04-30 02:25:55 +0900

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,40 +1,9 @@
-const colors = {
-  white: '#ffffff',
-  black: '#000000',
-  grey_0: '#d5d5d5',
-  grey_1: '#a6a6a6',
-  red: '#e84118',
-  blue: '#3679fe',
-  pastel_pink: '#fac8af',
+export const theme ={
+    background: '#fac8af',  //메인색(pastel_pink) 
+    text1: '#000000',       //글자색(검정)
+    text2: '#8d8d8d',       //placeholder색(연회색)
+    red: '#e84118',
+    white: '#ffffff',       //INPUT Component 내부색
+    blue: '#3679fe',
 };
 
-export const theme = {
-  background: colors.white,
-  text: colors.black,
-  errorText: colors.red,
-
-  imageBackground: colors.grey_0,
-  imageButtonBackground: colors.grey_1,
-  imageButtonIcon: colors.white,
-
-  label: colors.grey_1, //포커스기능이 있는 입력창 색깔
-  inputPlaceholder: colors.grey_1,
-  inputBorder: colors.grey_1,
-  inputDisabledBackground: colors.grey_0,
-
-  buttonBackground: colors.pastel_pink, //버튼 색깔
-  buttonTitle: colors.white,
-  buttonUnfilledTitle: colors.pastel_pink,
-  buttonLogout: colors.red,
-  headerTintColor: colors.black,
-  tabActiveColor: colors.pastel_pink,
-  tabInactiveColor: colors.grey_1,
-
-  spinnerBackground: colors.black,
-  spinnerIndicator: colors.white,
-
-  listBorder: colors.grey_0,
-  listTime: colors.grey_1,
-  listDescription: colors.grey_1,
-  listIcon: colors.black,
-};
