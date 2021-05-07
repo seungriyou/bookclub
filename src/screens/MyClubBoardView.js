080c30e781d234015c73a0e1d6444fa44a90a4d9
JWH <43639741+myatta@users.noreply.github.com>
2021-05-08 01:51:50 +0900

    Board View 관련 - 댓글 인터페이스 추가

--- src/screens/MyClubBoardView.js
+++ src/screens/MyClubBoardView.js
@@ -5,12 +5,15 @@ import ReplyInput from '../components/ReplyInput';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import {MaterialCommunityIcons} from '@expo/vector-icons';
 import {BoardTitle, BoardContent, BoardInfo} from '../components/BoardInfo';
+import BoardCommentList from '../components/BoardCommentList';
 
 const Container=styled.View`
     flex: 1;
     flex-direction: column;
     background-color: ${({theme})=>theme.appBackground};
     align-items: center;
+    padding-bottom: 100px;
+    justify-content: flex-start;
 `;
 
 const Containerforreply=styled.View`
@@ -36,6 +39,52 @@ const Line=styled.View`
     height:2px;
     background-color: ${({theme})=>theme.line};
 `;
+
+const tempData = {
+    "title": "05월 정기모임",
+    "writer_name": "관리자",
+    "upload_date": "2021/05/05",
+    "comment_cnt": 6,
+    "comment": [
+      {
+        "id": 1,
+        "writer_name": "멤버A",
+        "content": "재미있었어요!",
+        "upload_date": "2021/05/05",
+      },
+      {
+        "id": 2,
+        "writer_name": "멤버B",
+        "content": "사진 공유 감사합니다 :)",
+        "upload_date": "2021/05/05",
+      },
+      {
+        "id": 3,
+        "writer_name": "멤버C",
+        "content": "확인했습니다~",
+        "upload_date": "2021/05/05",
+      },
+      {
+        "id": 4,
+        "writer_name": "멤버D",
+        "content": "감사합니당!",
+        "upload_date": "2021/05/05",
+      },
+      {
+        "id": 5,
+        "writer_name": "멤버E",
+        "content": "확인했어요! 좋은 주말 되세요~",
+        "upload_date": "2021/05/05",
+      },
+      {
+        "id": 6,
+        "writer_name": "멤버F",
+        "content": "다음 모임에서 뵙겠습니다!",
+        "upload_date": "2021/05/06",
+      }
+    ]
+}
+
 
 
 const MyClubBoardView=({ navigation })=>{
@@ -93,7 +142,7 @@ const MyClubBoardView=({ navigation })=>{
             extraScrollHeight={20}
         >
         <Container>
-        <Container>
+        
             <List width={width}>
                 <BoardTitle 
                     title="[공지]4/12 오프라인 모임 안내" 
@@ -106,10 +155,11 @@ const MyClubBoardView=({ navigation })=>{
                 />
                 <Line />
                 <BoardContent 
-                    content="공지의 내용입니다. 긴 내용을 임의로 입력해보겠습니다." 
+                    content="공지의 내용입니다." 
                 />    
+                <BoardCommentList postInfo={tempData}></BoardCommentList>
             </List>
-        </Container>
+        
 
         <Containerforreply>
             <Listforreply width={width}>
