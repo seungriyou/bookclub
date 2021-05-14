255563b9aad3c0465efc1cb79c688f50c6a16a62
JWH <werrya2@naver.com>
2021-05-14 15:24:28 +0900

    Club.js 수정

--- src/screens/Club.js
+++ src/screens/Club.js
@@ -1,18 +1,82 @@
 import React, { useContext, useState, useEffect } from 'react';
 import styled from 'styled-components/native';
-import { Alert, FlatList, Text, Modal } from 'react-native';
+import { Alert, Dimensions, FlatList, StyleSheet, Text, Modal } from 'react-native';
 import { getClubInfo, DB, getCurrentUser, clubSignUpWaiting } from '../utils/firebase';
 import { Button } from '../components';
 import { ProgressContext } from '../contexts';
+import { theme } from '../theme';
+
 
 
 const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.background};
+  align-items: center;
+  justify-content: center;
+  padding: 0 20px;
+  width: ${({ width }) => width}px;
+  border-Width: 3px;
+  border-Color: ${({ theme }) => theme.appBackground};
+
+`;
+
+
+const TitleCon=styled.View`
+  background-color: ${({ theme }) => theme.background};
+  border-Width: 2.5px;
+  border-Color: ${({ theme }) => theme.appBackground};
+  min-width: ${({ width }) => (width)*0.6}px;
+  max-width: ${({ width }) => (width)*0.7}px;
   justify-content: center;
   align-items: center;
-  padding: 0 20px;
+  marginBottom: 50px;
+  border-radius: 10px;
 `;
+
+const AllCon=styled.View`
+  background-color: ${({ theme }) => theme.background};
+  border-Width: 2.5px;
+  border-Color: ${({ theme }) => theme.appBackground};
+  width: ${({ width }) => (width)*0.85}px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  paddingLeft: 10px;
+  paddingRight: 10px;
+  paddingBottom: 7px;
+  marginBottom: 50px;
+  border-radius: 10px;
+`;
+
+const Line=styled.View`
+  background-color: ${({ theme }) => theme.separator};
+  height: 0.8px;
+  width: ${({ width }) => ((width)*0.8)-10}px;
+`;
+
+const styles = StyleSheet.create({
+  First: {
+    fontSize: 28,
+    color: theme.text,
+    fontWeight: "bold",
+    padding: 10,
+  },
+  Second:{
+    fontSize: 16,
+    color: theme.text,
+    paddingTop: 14,
+    paddingBottom: 7,
+    paddingLeft: 7,
+  },
+  Img: {
+    width: 80,
+    height: 80,
+    borderRadius: 50,
+  },
+});
+
+
+
 
 const Club = ({ navigation, route }) => {
   const { spinner } = useContext(ProgressContext);
@@ -30,6 +94,9 @@ const Club = ({ navigation, route }) => {
   const [waiting, setWaiting] = useState(false);
 
   const user = getCurrentUser();
+
+  const width = Dimensions.get('window').width;
+
 
   const getClub = async () => {
     try {
@@ -90,13 +157,24 @@ const Club = ({ navigation, route }) => {
   };
 
   return (
-    <Container>
-      <Text style={{ fontSize: 24 }}>ID: {route.params?.id}</Text>
-      <Text style={{ fontSize: 24 }}>Title: {route.params?.title}</Text>
-      <Text style={{ fontSize: 12 }}>{title}</Text>
-      <Text style={{ fontSize: 12 }}>{leader.name}</Text>
-      <Text style={{ fontSize: 12 }}>{region}</Text>
-      <Text style={{ fontSize: 12 }}>{maxNumber}</Text>
+    <Container width={width}>
+      
+      <TitleCon width={width}>
+        <Text style={styles.First}>{title}</Text>
+      </TitleCon>
+      <AllCon width={width}>
+
+      <Text style={styles.Second}>소개글의 위치입니다. 다들 행복한 독서되세요.</Text>
+      <Line width={width}></Line>
+      <Text style={styles.Second}>관리자:  {leader.name}</Text>
+      <Line width={width}></Line>
+      <Text style={styles.Second}>지역구:  {region}</Text>
+      <Line width={width}></Line>
+      <Text style={styles.Second}>모임형태:  모임형태의 위치입니다.</Text>
+      <Line width={width}></Line>
+      <Text style={styles.Second}>최대인원:  {maxNumber}</Text>
+
+      </AllCon>
       <Button
         title="가입신청"
         onPress={signUpClub}
